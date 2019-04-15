a6aad4b04c5357f83b74fb7a15e14892eefecb02
Srini Karlekar <skarlekar@yahoo.com>
2019-04-15 03:59:05 +0000

    Parameterized tester

--- test-scaling.sh
+++ test-scaling.sh
@@ -1,1 +1,3 @@
-ab -n 100000 -c 100 -s 120 http://bean-counter-load-balancer-1485864658.us-east-1.elb.amazonaws.com/
+export DNS=$(aws elbv2 describe-load-balancers | jq '.LoadBalancers[] | if .LoadBalancerName == "bean-counter-load-balancer" then .DNSName else null end' | grep -v null | sed "s/\"//g")
+
+ab -n 100000 -c 100 -s 120 http://$DNS/
