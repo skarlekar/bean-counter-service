cc11636cb27cd3744a8c1d781b887ba502deae98
Srini Karlekar <skarlekar@yahoo.com>
2019-04-19 04:07:19 +0000

    Generic Fargate ALB

--- _create-load-balancer.sh
+++ _create-load-balancer.sh
@@ -1,2 +1,2 @@
-aws elbv2 create-load-balancer --name Bean-Counter-ALB --subnets $SUBNET1 $SUBNET2 --security-groups $SECURITYGROUP
+aws elbv2 create-load-balancer --name My-Fargate-ALB --subnets $SUBNET1 $SUBNET2 --security-groups $SECURITYGROUP
 
--- _create-target-group.sh
+++ _create-target-group.sh
@@ -1,1 +1,1 @@
-aws elbv2 create-target-group --name Bean-Counter-TG --protocol HTTP --port 80 --target-type ip --vpc-id $VPC
+aws elbv2 create-target-group --name My-Fargate-TG --protocol HTTP --port 80 --target-type ip --vpc-id $VPC
